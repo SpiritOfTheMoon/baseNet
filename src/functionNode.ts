0d8724882bcaa80c4cc46cc5eec5640927fc2aff
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-02-06 11:18:45 +0500

    функции активации

---
+++ src/functionNode.ts
@@ -0,0 +1,11 @@
+export const sigmoid = (x: number): number => {
+    return 1 / (1 + Math.exp(-x));
+}
+
+export const hyperbolicTangent = (x: number): number => {
+    return (Math.exp(2 * x) - 1) / (Math.exp(2 * x) + 1)
+}
+
+export const linear = (x: number): number => {
+    return x
+}
