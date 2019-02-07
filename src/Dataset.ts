67790fbcf187dd9b2a33af36b40e4f4657c4e5d1
SpiritOfTheMoon <nya19999@gmail.com>
2019-02-07 11:46:05 +0500

    Созданы классы сети

---
+++ src/Dataset.ts
@@ -0,0 +1,8 @@
+export class Dataset {
+    public set: number[];
+    public answer: number
+    constructor(set: number[], answer: number) {
+        this.set = set;
+        this.answer = answer;
+    }
+}
---
+++ src/Network.ts
@@ -0,0 +1,53 @@
+import { GraphNode } from "./GraphNode";
+import { Dataset } from "./Dataset";
+
+export class Network {
+    public graph: GraphNode[];
+    public inputNode: number[];
+    public outputNode: number[];
+    public dataset: Dataset[];
+
+    constructor() {
+        this.graph = [];
+        this.inputNode = [];
+        this.outputNode = [];
+        this.dataset = [];
+    }
+
+    public graphCreate() {
+        for (let i: number = 0; i < 5; i++) {
+            this.graph.push(new GraphNode(i));
+        }
+        let a = Math.random();
+        this.graph[0].setEdge(2, [a, a]);
+        a = Math.random();
+        this.graph[0].setEdge(3, [a, a]);
+        a = Math.random();
+        this.graph[1].setEdge(2, [a, a]);
+        a = Math.random();
+        this.graph[1].setEdge(3, [a, a]);
+        a = Math.random();
+        this.graph[2].setEdge(4, [a, a]);
+        a = Math.random();
+        this.graph[3].setEdge(4, [a, a]);
+        this.graph[2].setEdge(0, [0]);
+        this.graph[2].setEdge(1, [0]);
+        this.graph[3].setEdge(0, [0]);
+        this.graph[3].setEdge(1, [0]);
+        this.graph[4].setEdge(2, [0]);
+        this.graph[4].setEdge(3, [0]);
+    }
+
+    public graphInit(i: number) {
+        for (let j: number = 0; j < this.graph.length; j++) {
+            this.graph[j].input = 0;
+            this.graph[j].output = 0;
+        }
+        for (let j: number = 0; j < this.inputNode.length; j++) {
+            this.graph[this.inputNode[j]].input = this.dataset[i].set[j];
+            this.graph[this.inputNode[j]].output = this.dataset[i].set[j];
+        }
+
+    }
+
+}
